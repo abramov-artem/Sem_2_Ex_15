29f576f4284143cbdc7a6025e5be4fae3240b539
abramov-artem <94751162+abramov-artem@users.noreply.github.com>
2022-03-21 20:56:47 +0300

    Create Ex-15\2'-var-1.c

---
+++ Ex-15\2'-var-1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+  int n, len, odd, k1=0, k2=0, flag=0;
+  int **graph[10][10];
+
+  FILE *fread = fopen("input.txt", "r");
+  fscanf(fread, "%d", &len);
+
+  for(int i=0; i<len; i++){
+    for(int j=0; j<len; j++){
+      fscanf (fread, "%d", &odd);
+      if(odd) graph[i][j]=1;
+      else graph[i][j]=0;
+    }
+  }
+  fclose(fread);
+
+  for (int i=0; i<len; i++){
+    for (int j=0; j<len; j++){
+      if (i>j && graph[i][j]==1){
+        k1++;
+      }
+      else if (i==j && graph[i][j]==1){
+        printf("Это псевдограф");
+        flag=1;
+        break;
+      }
+    }
+  }
+  if (flag!=1 && k1>(len-1)*(len-2)/2) printf("Граф связный");
+  else printf("Граф несвзяный");
+}
